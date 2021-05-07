497d61fe717ea5c93951e7c776a504ee957f8572
gianmarioale <giammi.alessio@gmail.com>
2021-05-07 16:03:42 +0200

    provi di stili sul file helloworld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div class="bg-red-500 text-center px-4">
+  <img alt="Vue logo" src="./assets/logo.png" />  <!-- tag html normale -->
+  <HelloWorld msg="Hello Vue 3 + Vite" /> <!-- tag html di vue -->
+  <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut esse fuga non praesentium eveniet optio suscipit magni modi voluptates consequuntur neque, exercitationem nihil cum culpa dignissimos quo veniam error!</p>
+  <p class="text-green-600 m-40 bg-yellow-300">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, natus sed voluptas ipsa cupiditate fugit. Praesentium earum molestiae tenetur pariatur quidem quos totam, est doloremque dolor deserunt aspernatur, commodi ipsa!</p>
+  <button class="p-4 bg-yellow-800 rounded text-gray-200 text-xl hover:bg-green-400 hover:text-white" > prova bottone </button>
+   <button class="bottone"> secondo bottone </button>
+
+  </div>
 </template>
 
 <script setup>
@@ -10,13 +17,3 @@ import HelloWorld from './components/HelloWorld.vue'
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 // src/main.js
+
+/** require si usa la parte server, import nella parte client. Sono equivalenti, ma i commandi sono leggermente difficili 
+ * il modulo di vue importa dei componenti 
+*/
 import { createApp } from 'vue'
 import App from './App.vue'
 import './css/style.css'
